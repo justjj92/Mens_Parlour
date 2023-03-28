58bcc1b6f24c20c21be9e312e8b2cb09ce21f47f
justjj92 <justin.jordan92@gmail.com>
2023-03-27 17:24:31 -0700

    Initial Commit

---
+++ Mens_Parlour/Mens_ParlourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mens_ParlourApp.swift
+//  Mens_Parlour
+//
+//  Created by J J on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Mens_ParlourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
